3b913a101e193d82c0e52860a62d7484e225bb85
Evgeniy <borovoyevgen@gmail.com>
2024-12-03 11:01:58 +0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Evgeniy Borovoy on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
